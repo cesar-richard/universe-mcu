b7ddd562c54ff6231677f37e1cd24972f0115566
Cesar Richard <cesar.richard@weezevent.com>
2020-04-05 23:06:20 +0200

    pretty

--- universe-mcu.ino
+++ universe-mcu.ino
@@ -1,62 +1,92 @@
 //https://github.com/morrissinger/ESP8266-Websocket to include
 #include <ESP8266WiFi.h>
+
 #include <WebSocketClient.h>
+
 #include <Ticker.h>
 
-const int pinArray[9] = {D0,D1,D2,D3,D4,D5,D6,D7,D8};
-int lastStateArray[9] = {0,0,0,0,0,0,0,0,0};
-const String nameArray[9] = {"blueLed","blue","green","black","white","yellow","red","gray","D8"};
+const int pinArray[9] = {
+  D0,
+  D1,
+  D2,
+  D3,
+  D4,
+  D5,
+  D6,
+  D7,
+  D8
+};
+int lastStateArray[9] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+const String nameArray[9] = {
+  "blueLed",
+  "blue",
+  "green",
+  "black",
+  "white",
+  "yellow",
+  "red",
+  "gray",
+  "D8"
+};
 const int interval = 10000;
-const char* ssid = "Licornes";
-const char* wifipassword = "UnicornPowaaaaa";
-char* wshost = "192.168.1.29";
+const char * ssid = "Licornes";
+const char * wifipassword = "UnicornPowaaaaa";
+char * wshost = "192.168.1.29";
 char path[] = "/";
 
 Ticker ticker;
 WebSocketClient webSocketClient;
 WiFiClient client;
 
-boolean handshakeFailed=0;
-
-void cb(String , String , String );
+void cb(String, String, String);
 void tick();
-void btnCheck(int, void*);
+void btnCheck(int, void * );
 void wsconnect();
 
-void cb(String event, String sensor, String state){
-  String str = "{\"event\":\""+event+"\",\"sensor\":\""+sensor+"\",\"state\":\""+state+"\",\"time\":"+(String)millis()+"}";
+void cb(String event, String sensor, String state) {
+  String str = "{\"event\":\"" + event + "\",\"sensor\":\"" + sensor + "\",\"state\":\"" + state + "\",\"time\":" + (String) millis() + "}";
   Serial.println("Sending : " + str);
   webSocketClient.sendData(str);
 }
 
-void tick(){
-   if (client.connected()) {
+void tick() {
+  if (client.connected()) {
     String data;
     /*webSocketClient.getData(data);    
     if (data.length() > 0) {
       Serial.println(data);
     }*/
-    cb("heartbeat","core","alive");
-   } else {
+    cb("heartbeat", "core", "alive");
+  } else {
     Serial.println("Client not connected");
     delay(1000);
     ESP.restart();
-   }
+  }
 }
 
-void btnCheck(int index, void (&callback)(String, String, String)){
+void btnCheck(int index, void( & callback)(String, String, String)) {
   int state = digitalRead(pinArray[index]);
   if (state != lastStateArray[index]) {
     if (state == LOW) {
-      callback("button",nameArray[index],"on");
+      callback("button", nameArray[index], "on");
     } else {
-      callback("button",nameArray[index],"off");
+      callback("button", nameArray[index], "off");
     }
     lastStateArray[index] = state;
   }
 }
 
-void wsconnect(){
+void wsconnect() {
   if (client.connect(wshost, 3000)) {
     Serial.println("Connected");
   } else {
