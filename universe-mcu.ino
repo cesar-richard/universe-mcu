2073a5b825cf93f435bf5c3d0b37268cd7d3324a
César Richard <cesar.richard@hds.utc.fr>
2020-04-05 13:40:34 +0200

    order

--- universe-mcu.ino
+++ universe-mcu.ino
@@ -25,6 +25,61 @@ boolean handshakeFailed=0;
 int redButtonState,blueButtonState,greenButtonState,blackButtonState,whiteButtonState,yellowButtonState = 0;
 int lastRedButtonState,lastBlueButtonState,lastGreenButtonState,lastBlackButtonState,lastWhiteButtonState,lastYellowButtonState = 0;
 
+void cb(String event, String sensor, String state){
+  String str = "{\"event\":\""+event+"\",\"sensor\":\""+sensor+"\",\"state\":\""+state+"\",\"time\":"+(String)millis()+"}";
+  Serial.println("Sending : " + str);
+  webSocketClient.sendData(str);
+}
+
+void tick(){
+   if (client.connected()) {
+    String data;
+    /*webSocketClient.getData(data);    
+    if (data.length() > 0) {
+      Serial.println(data);
+    }*/
+    cb("heartbeat","core","alive");
+   } else {
+    Serial.println("Client not connected");
+    delay(1000);
+    ESP.restart();
+   }
+}
+
+void btnCheck(int pin, int* lastState, String name, void (&callback)(String, String, String)){
+  int state = digitalRead(pin);
+  if (state != *lastState) {
+    
+    if (state == LOW) {
+      callback("button",name,"on");
+    } else {
+      callback("button",name,"off");
+    }
+    *lastState = state;
+    delay(20);
+  }
+}
+
+void wsconnect(){
+  if (client.connect(wshost, 3000)) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Connection failed.");
+    ESP.restart();
+  }
+  // Handshake with the server
+  webSocketClient.host = wshost;
+  webSocketClient.path = path;
+  if (webSocketClient.handshake(client)) {
+    Serial.println("Handshake successful");
+    digitalWrite(blueLedPin, HIGH);
+  } else {
+    Serial.println("Handshake failed.");
+    delay(1000);
+    ESP.restart();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, wifipassword);
@@ -62,59 +117,4 @@ void loop() {
   btnCheck(blackButtonPin, &lastBlackButtonState, "black", cb);
   btnCheck(whiteButtonPin, &lastWhiteButtonState, "white", cb);
   btnCheck(yellowButtonPin, &lastYellowButtonState, "yellow", cb); 
-}
-
-void tick(){
-   if (client.connected()) {
-    String data;
-    /*webSocketClient.getData(data);    
-    if (data.length() > 0) {
-      Serial.println(data);
-    }*/
-    cb("heartbeat","core","alive");
-   } else {
-    Serial.println("Client not connected");
-    delay(1000);
-    ESP.restart();
-   }
-}
-
-void cb(String event, String sensor, String state){
-  String str = "{\"event\":\""+event+"\",\"sensor\":\""+sensor+"\",\"state\":\""+state+"\",\"time\":"+(String)millis()+"}";
-  Serial.println("Sending : " + str);
-  webSocketClient.sendData(str);
-}
-
-void btnCheck(int pin, int* lastState, String name, void (&callback)(String, String, String)){
-  int state = digitalRead(pin);
-  if (state != *lastState) {
-    
-    if (state == LOW) {
-      callback("button",name,"on");
-    } else {
-      callback("button",name,"off");
-    }
-    *lastState = state;
-    delay(20);
-  }
-}
-
-void wsconnect(){
-  if (client.connect(wshost, 3000)) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Connection failed.");
-    ESP.restart();
-  }
-  // Handshake with the server
-  webSocketClient.host = wshost;
-  webSocketClient.path = path;
-  if (webSocketClient.handshake(client)) {
-    Serial.println("Handshake successful");
-    digitalWrite(blueLedPin, HIGH);
-  } else {
-    Serial.println("Handshake failed.");
-    delay(1000);
-    ESP.restart();
-  }
 }
