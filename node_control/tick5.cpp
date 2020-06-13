8bd4d114228da5a109cc171395443257302520e3
Cesar <licorne@utc.fr>
2020-06-13 20:24:01 +0200

    restart
    Signed-off-by: Cesar <licorne@utc.fr>

--- node_control/tick5.cpp
+++ node_control/tick5.cpp
@@ -1,9 +1,3 @@
 void customtick5(WiFiClient client, void (&cb)(String, String, String)){
-   if (client.connected()) {
-    cb(F("heartbeat"),F("core"),F("alive"));
-   } else {
-    Serial.println(F("Client not connected"));
-    delay(1000);
-    ESP.restart();
-   }
+  cb(F("heartbeat"),F("core"),F("alive"));
 }
